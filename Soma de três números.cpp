b6689b2f75f535020b37b2a6b41c3b3cc9a9a5c6
Hibrael <70974067+Hibrael@users.noreply.github.com>
2025-02-17 19:44:30 -0300

    Add files via upload
    Exercício 01 - soma aritmética de três números

---
+++ Soma de três números.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main ( )
+{
+ float n1, n2, n3;
+ float media;
+ cout << "Digite o primeiro valor: ";
+ cin >> n1;
+ cout << "segundo: ";
+ cin >> n2;
+ cout << "ultimo valor: ";
+ cin >> n3;
+ media = (n1 + n2 + n3) / 3; 
+ cout << "A media dos numeros e : " << media;
+}
